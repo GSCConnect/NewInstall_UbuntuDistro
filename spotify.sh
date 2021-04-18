2b0b5a4d04cbc3f55924a8703c4b2a6bb0462022
GSCConnect <82615884+GSCConnect@users.noreply.github.com>
2021-04-18 17:10:23 -0500

    Install Spotify

---
+++ spotify.sh
@@ -0,0 +1,9 @@
+#! /bin/bash 
+
+#Add pubkey & repository
+
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+#Install
+sudo apt-get update && sudo apt-get install spotify-client
