b4c366841b25a3384ee8f279e547d61dd8788864
GSCConnect <82615884+GSCConnect@users.noreply.github.com>
2021-05-26 13:14:38 -0500

    yeh boy

---
+++ afterinstall.sh
@@ -0,0 +1,15 @@
+#! /bin/bash/
+
+sudo apt update -y
+sudo apt upgrade -y
+
+#MUST HAVE CLI RELATED TOOLS
+sudo apt install terminator lolcat fortune cowsay lynis debsecan snapd python3-pip iptraf-ng htop whois net-tools iwconfig glances adb fastboot testdisk android-sdk locate ncdu -y
+
+#Stupid shit I like installing
+sudo apt install plank gnome-disk-utility gparted minder telegram-desktop yakuake sl cmatrix-y
+
+#PC HARDENING
+sudo apt install fail2ban -y
+
+cowsay "YOUR INSTALLATION IS DONE" | lolcat
