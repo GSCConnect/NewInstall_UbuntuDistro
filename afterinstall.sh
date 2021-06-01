ee6f4e9f638030417616de186389ab696cdcefa6
GSCConnect <82615884+GSCConnect@users.noreply.github.com>
2021-05-31 21:30:10 -0500

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -7,9 +7,17 @@ sudo apt upgrade -y
 sudo apt install terminator lolcat fortune cowsay lynis debsecan snapd python3-pip iptraf-ng htop whois net-tools iwconfig glances adb fastboot testdisk android-sdk locate ncdu -y
 
 #Stupid shit I like installing
-sudo apt install plank gnome-disk-utility gparted minder telegram-desktop yakuake sl cmatrix-y
+sudo apt install plank gnome-disk-utility gparted minder telegram-desktop yakuake sl cmatrix -y
 
 #PC HARDENING
 sudo apt install fail2ban -y
 
+sudo snap install norpass termius-app
+
 cowsay "YOUR INSTALLATION IS DONE" | lolcat
+
+
+# other shit
+
+sudo apt install etcd 
+
