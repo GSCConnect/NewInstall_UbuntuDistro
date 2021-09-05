6afccd764c73cb047437f7f11f82bfb858a1b6d8
gscconnect <raulharo95@gmail.com>
2021-09-05 14:11:31 -0500

    Fixed the scripts, un poco mas

--- Scripts/ExtraSoftware.sh
+++ Scripts/ExtraSoftware.sh
@@ -2,21 +2,21 @@
 
 echo -e "Adding the Spotify Repositories to /etc/sources.list"
 #Add pubkey & repository
-echo -------> 
+echo  "--------------------------------------------------------------------------"
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sleep 4
 
 echo -e "Done"
 Sleep 6
-echo ----------------------------------------------------------------------------
+echo "----------------------------------------------------------------------------"
 echo -e "Installing Spotify... Please wait"
 #Install
 sudo apt-get update && sudo apt-get install spotify-client
 
 echo -e  "Spotify has been installed, I hope LOL"
 sleep 5
-echo ----------------------------------------------------------------------------
+echo "----------------------------------------------------------------------------"
 echo -e "NordVPN Install Script Initializing..."
 
 sh <(wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh)
@@ -41,7 +41,7 @@ echo alias fun='fortune | cowsay | lolcat' >> ~/.bash
 # all you need to do is write the following command: sudo usermod -aG nordvpn $USER and then reboot your device.
 
 sleep 5
-echo ---------------------------------------------------------------------
+echo "---------------------------------------------------------------------"
 #Lof into NordVPN 
 nordvpn login
 
