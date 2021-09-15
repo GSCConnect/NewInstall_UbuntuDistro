7a9dd8529c452b9d7b757c0e59fe8d4b196b17f8
GSCConnect <82615884+GSCConnect@users.noreply.github.com>
2021-09-15 01:03:33 +0000

    Removed snap from script, fuck you snap

--- Scripts/InitialScript.sh
+++ Scripts/InitialScript.sh
@@ -8,19 +8,18 @@ echo -------------------------------------------------------------------------
 echo -e "Done"
 echo -e "Installing CLI Tools"
 #MUST HAVE CLI RELATED TOOLS
-sudo apt install terminator lynis debsecan snapd python3-pip iptraf-ng htop whois net-tools iwconfig glances adb fastboot testdisk android-sdk locate ncdu -y
+sudo apt install terminator lynis debsecan python3-pip iptraf-ng htop whois net-tools iwconfig glances adb fastboot testdisk android-sdk locate ncdu -y
 echo -e "Done"
 echo -------------------------------------------------------------------------
 echo -e "Installing dumb shit I like"
 #I just like pipelining this commands
-sudo apt install lolcat fortune cowsay fail2ban
+sudo apt install lolcat fortune cowsay fail2ban tripwire gnome-disk-utility plank lynis git selinux-* terminator clamav-* rkhunter libpam-google-authenticator  libpam-tmpdir libpam-passwdqc libpam-ssh libpam-python libpam-mount libpam-cracklib libpam-mklocaluser libpam-systemd ncdu testdisk --fix-missing -y
 
 #Stupid shit I like installing
 sudo apt install plank gnome-disk-utility gparted minder telegram-desktop yakuake sl cmatrix -y
 
-echo -e "Snap Applications"
-
-sudo snap install norpass termius-app
+#echo -e "Snap Applications"
+#sudo snap install norpass termius-app
 
 echo -e "Installation Complete"
 
